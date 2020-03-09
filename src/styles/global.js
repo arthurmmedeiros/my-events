5a0fb6d518ebd8cc4d0ad1cfa262be457cb35d8e
ammedeiros <amedeiros@symplicity.com>
2020-03-09 16:53:07 -0300

    changing to products

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
+import background from '../assets/images/background.svg';
+
 export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
     * {
         maring: 0;
         padding: 0;
@@ -8,22 +12,33 @@ export default createGlobalStyle`
         box-sizing: border-box;
     }
 
-    html, body, #root {
-        min-height: 100%;
-    }
-
     body {
-        background: #7159c1;
+        background: #191920 url(${background}) no-repeat center top;
         -webkit-font-smoothin: antialiased !important;
     }
 
     body, input, button {
         color: #222;
+        font: 14px Roboto, sans-serif;
         font-size: 14px;
         font-family: Arial, Helvetica, sans-serif;
+    }
+
+    #root {
+        max-width: 1020px;
+        margin: 0 auto;
+        padding: 0 20px 50px;
     }
 
     button {
         cursor: pointer;
     }
+
+
+
+
+
+
+
+
 `;
